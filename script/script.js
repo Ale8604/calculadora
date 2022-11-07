61f8fdcf55c1ee30d97982e2075945cb51fc8d12
Alejandro <alejandrosotelo080104@gmail.com>
2022-11-07 12:13:37 -0500

    mode dark

--- script/script.js
+++ script/script.js
@@ -118,3 +118,20 @@ function resolver(){
   resetear();
   resultado.textContent = res;
 }
+
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    console.log(toggle)
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+});
